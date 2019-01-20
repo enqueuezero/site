195e0da989a199702604580cbfd9213acbd99c55
soasme <soasme@gmail.com>
2019-01-20 15:45:41 +1300

    add subscribe cn list.

---
+++ .vuepress/components/SubscribeCNList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="subscribe-cn-list">
+    <p>以上资料的全文链接都已给出，如果想看文章的精炼笔记，可以查看：<a href="https://enqueuezero.com/category/techshack.html">Techshack Weekly</a>。也欢迎加入 <a href="https://t.me/techshack">中文 Telegram Channel</a> 或者关注 <a href="https://twitter.com/techshackweekly">@techshackweekly</a>。如果你更喜欢查收邮件，可以提交你的邮箱地址到列表（仅用于发送本系列的更新，不会发送其它信息）：
+    </p>
+    <!-- Begin MailChimp Signup Form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+    <div id="mc_embed_signup">
+    <form action="https://soasme.us17.list-manage.com/subscribe/post?u=617c1cb80ba1b0cbf828df387&amp;id=10442a8ee6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        <label for="mce-EMAIL">Subscribe to our mailing list</label>
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_617c1cb80ba1b0cbf828df387_10442a8ee6" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+    </form>
+    </div>
+    <p>
+      最后，如果你觉得 (我做的事情是有价值的 && 对自己的学习有帮助 && 不介意每个月养我 $1), 请前往 <a href="https://www.patreon.com/enqueuezero">Patreon</a> 进行赞助。捐赠到的钱将会被我用于维护域名，购买语法检查服务，发送邮件等等。感谢你的大力支持。
+    </p>
+  </div>
+</template>
+
+<style>
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif;  width:250px;}
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+      We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
